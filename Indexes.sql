b7d4e7e0d90975b3579157b6eb4303ddecd7b111
Bashat Parween <68892857+bashat450@users.noreply.github.com>
2025-04-21 18:05:40 +0530

    Add files via upload

---
+++ Indexes.sql
@@ -0,0 +1,35 @@
+Use BashatParween;
+
+/*
+An index in SQL Server is a database object that helps speed up the retrieval of rows from a table.
+Think of it like an index in a book — instead of reading every page, you go directly to the page number listed.
+**Improve query performance.
+**Reduce the amount of data SQL Server needs to scan.
+**Useful for searching, filtering, sorting, and joining operations.
+*/
+select * from Employees;
+--CLUSTERED INDEX  --With Clustered Index on ID:  [ 1 | 2 | 3 | 4 | 5 ]
+--Automatically created on the primary key unless specified otherwise.
+
+CREATE CLUSTERED INDEX idx_Employee_Reg_id
+ON Employees (RegId);
+
+/*
+******************Non-Clustered Index
+You can create multiple non-clustered indexes on a table.
+Useful for searching, filtering, and joining on non-primary key columns.
+*/
+CREATE NONCLUSTERED INDEX idx_emp_fullname
+ON Employees (FullName);
+
+
+---************Unique Index
+--Ensures that no duplicate values are entered in the column(s).
+CREATE UNIQUE INDEX idx_unique_email
+ON Employees (EmailId);
+
+
+
+sp_helpindex 'Employees';
+
+
