493d74b9ba0a14ab3a89893a504089da0f4476ae
Bashat Parween <68892857+bashat450@users.noreply.github.com>
2025-04-30 15:57:18 +0530

    Update SQLTest_BashatParween.sql

--- SQLTest_BashatParween.sql
+++ SQLTest_BashatParween.sql
@@ -1,8 +1,8 @@
 Use BashatParween;
 select * from [dbo].[order];
---1)
+--1) Get the total number of records in the orders table. 
 select Count(*) As TotalRecords From [order];
--- 2)
+-- 2) Retrieve unique job titles from the employees table.
 alter table Employees Add JobTitle varchar(100);
 select * from [dbo].[Employees];
 Update Employees Set JobTitle = 'Manager' Where RegId = 1;
@@ -18,10 +18,10 @@ Update Employees Set JobTitle = 'Staff' Where RegId = 11;
 Update Employees Set JobTitle = 'Staff' Where RegId = 12;
 
 Select Distinct JobTitle from Employees;
--- 3)
+-- 3) Find employees whose salary is more than 50000.
 Select * from Employees 
 Where Salary > 50000;
---4)
+--4) Write query all types of joins
 --i) Inner Join (Matching records from both tables)[dbo].[Department]
 Select e.FullName  as EmployeeName,
 d.DepName as DepartmentName
@@ -59,14 +59,14 @@ From Employees e
 Cross Join
 Department d ;
 
---6)
+--6) Find the second highest salary from the employees table. 
 select Distinct Salary
 From Employees
 Order By Salary desc
 Offset 1 row 
 fetch
 next 1 row only;
---7)
+--7) Display employees along with their department names using a JOIN.
 Select e.FullName as EmployeeName,
 d.DepName as DepartmentName 
 From Employees e
@@ -74,20 +74,20 @@ Join
 Department d
 On e.RegId = d.DepId;
 
---8)
+--8) Group employees by department and get the average salary per department. 
 Select DepId as Department_Id,
 Avg(Salary) as [Average salary]
 From Employees
 Group By DepId;
 
---9)
+--9) Get the top 3 highest paid employees.
 select Distinct top 3  
 FullName as EmployeeName,
 Salary 
 from Employees
 Order By Salary desc;
 
---10)
+--10) . List departments that have more than 5 employees.
 select DepId as Department_Id, 
 Count(*) as TotalEmployees
 From Employees
@@ -103,7 +103,7 @@ On e.RegId = d.DepId
 Group By DepName
 Having Count(RegId) > 5 ;
 
---13)
+--13)  Fetch employees who do not have any Department
 SELECT e.RegId as Employee_Id,
 e.FullName as EmployeeName,
 d.DepName as DepartmentName
@@ -113,13 +113,13 @@ Department d
 ON e.RegId = d.DepId
 WHERE d.DepId IS NULL; 
 
---14)
+--14)  Find employees who joined in the last 30 days.
 select FullName as EmployeeName,
 Currentdate as [Joining Date]
 From Employees 
 Where CurrentDate  >= DateADD(Day, -30, GetDate());
 
---15)
+--15) Retrieve the employee with the nth highest salary. (e.g. 3rd highest); 
 select FullName as EmployeeName,
 Salary as TotalSalary
 from Employees s1
@@ -136,23 +136,23 @@ Where Salary >
 (Select Avg(Salary)
 From Employees);
 
---17)
+--17)  Write a Query Concatenate of two  column.
 select Concat(FirstName, '  ', LastName) from Student3;
 
---18)
+--18)   Find pattern matching ‘A’ ?
 Select FullName as EmployeeName From Employees
 where FullName Like 'A%';
 Select FullName as EmployeeName From Employees
 where FullName Like '%h';
 Select FullName as EmployeeName From Employees
 where FullName Like '_a';
---19)
+--19)  Write a query  Delete,Drop and truncate.
 Drop table Students2;
 Truncate Table Students2;
 insert into Students2 values (1,'Pooja');
 Delete From Students2 Where StudentID =1;
 
---20)
+--20) Write  a query insert, update,Delete query using stored procedure.
 create procedure spGetEmployees
 --with encryption
 as
